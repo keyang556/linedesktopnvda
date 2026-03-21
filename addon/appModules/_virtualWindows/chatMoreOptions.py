5df80eee5f516b75a3374a26421d50f412d2b6a0
Keyang556 <65295310+keyang556@users.noreply.github.com>
2026-03-21 19:01:17 +0800

    test

---
+++ addon/appModules/_virtualWindows/chatMoreOptions.py
@@ -0,0 +1,74 @@
+from .._virtualWindow import VirtualWindow
+from .._utils import ocrGetText, message
+from logHandler import log
+
+import re
+
+_CJK_CHAR = (
+	r'[\u2E80-\u9FFF\uF900-\uFAFF'
+	r'\U00020000-\U0002A6DF\U0002A700-\U0002EBEF\U00030000-\U000323AF]'
+)
+_CJK_SPACE_RE = re.compile(
+	r'(?<=' + _CJK_CHAR + r') (?=' + _CJK_CHAR + r')'
+)
+
+def _removeCJKSpaces(text):
+	return _CJK_SPACE_RE.sub('', text)
+
+
+class ChatMoreOptions(VirtualWindow):
+	title = '更多選項'
+
+	@staticmethod
+	def isMatchLineScreen(obj):
+		return False
+
+	def __init__(self, popupRect):
+		self.elements = []
+		self.pos = -1
+		self.popupRect = popupRect
+		left, top, right, bottom = popupRect
+		width = right - left
+		height = bottom - top
+		if width > 0 and height > 0:
+			ocrGetText(left, top, width, height, self._onOcrResult)
+		message(self.title)
+
+	def makeElements(self):
+		pass
+
+	def _onOcrResult(self, result):
+		if not result or isinstance(result, Exception):
+			log.debug(f"LINE: ChatMoreOptions OCR error: {result}")
+			return
+
+		text = getattr(result, 'text', '') or ''
+		text = _removeCJKSpaces(text.strip())
+		lines = [l.strip() for l in text.split('\n') if l.strip()]
+
+		if not lines:
+			log.debug("LINE: ChatMoreOptions OCR returned no lines")
+			return
+
+		left, top, right, bottom = self.popupRect
+		height = bottom - top
+		itemHeight = height / len(lines)
+		centerX = (left + right) // 2
+
+		for i, line in enumerate(lines):
+			itemCenterY = int(top + itemHeight * i + itemHeight / 2)
+			self.elements.append({
+				'name': line,
+				'role': None,
+				'clickPoint': (centerX, itemCenterY)
+			})
+
+		log.info(f"LINE: ChatMoreOptions found {len(self.elements)} items: {[e['name'] for e in self.elements]}")
+
+		if self.elements:
+			self.pos = 0
+			self.show()
+
+	def click(self):
+		super().click()
+		VirtualWindow.currentWindow = None
