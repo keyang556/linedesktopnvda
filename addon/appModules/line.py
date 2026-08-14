f3fb0634ba0ad029a58c97737cb4855a29eb679a
Keyang556 <65295310+keyang556@users.noreply.github.com>
2026-08-14 10:09:02 +0800

    update model
    gm3.7f

--- addon/appModules/line.py
+++ addon/appModules/line.py
@@ -2220,9 +2220,10 @@ _IMAGE_DESCRIPTION_NVIDIA_DEFAULT_MODEL = "mistralai/mistral-medium-3.5-128b"
 _IMAGE_DESCRIPTION_AVAILABLE_MODELS = (
 	"gemini-3-flash-preview",
 	"gemini-3.1-flash-lite",
+	"gemini-3.5-flash",
 	"gemini-3.5-flash-lite",
 	"gemini-3.6-flash",
-	"gemini-3.5-flash",
+	"gemini-3.7-flash",
 	"gemini-flash-latest",
 	"gemini-flash-lite-latest",
 	"gemini-2.5-flash",
